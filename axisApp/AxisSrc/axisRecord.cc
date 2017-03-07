870bb5d1954c7b1db71d30c70ef0d4d313990866
Torsten Bögershausen <torsten.bogershausen@esss.se>
2017-03-07 10:10:04 +0100

    EthercatMC: Add EthercatMChome.template
    Move all parameters for homing into an own template.
    Now we have parameters for
    - Velocity to the CAM
    - Velocity from the CAM
    - Acceleration
    - Deceleration
    - Homing position
    - Homing procedure (this is now taken out of EthercatMC.template)
    
    Set RDBD to 0, since the record will set it to MRES
    Set VERS to 10.0.7

--- axisApp/AxisSrc/axisRecord.cc
+++ axisApp/AxisSrc/axisRecord.cc
@@ -185,7 +185,7 @@ USAGE...        Motor Record Support.
  *                    Changed error checks from dial to user limits.
  */                                                          
 
-#define VERSION 10.000006
+#define VERSION 10.000007
 
 #include    <stdlib.h>
 #include    <string.h>
