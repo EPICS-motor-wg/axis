304935a728d06a5cbc2b8dc9b8c76257181e4e3c
Torsten Bögershausen <torsten.bogershausen@esss.se>
2017-10-13 13:38:04 +0200

    Increase VERS to 10.1.5

--- axisApp/AxisSrc/axisRecord.cc
+++ axisApp/AxisSrc/axisRecord.cc
@@ -185,7 +185,7 @@ USAGE...        Motor Record Support.
  *                    Changed error checks from dial to user limits.
  */                                                          
 
-#define VERSION 10.001004
+#define VERSION 10.001005
 
 #include    <stdlib.h>
 #include    <string.h>
